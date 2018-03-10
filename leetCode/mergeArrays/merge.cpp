aed66c2e57d8887e891fc9f0676f0bced2d1beb3
Sahil Sharma <sahil.sharma@sjsu.edu>
2018-03-09 20:30:58 -0800

    commited on March 9th

--- leetCode/mergeArrays/merge.cpp
+++ leetCode/mergeArrays/merge.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -13,38 +14,83 @@ using namespace std;
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n);
 
 int main() {
-	int m = 9;
-	int n = 4;
+	int size = 6;
+	int m = 3;
+	int n = 3;
 
-	vector<int> nums1(m);
-	nums1[0] = 1;
+	vector<int> nums1(size);
+	nums1[0] = 4;
 	nums1[1] = 5;
-	nums1[2] = 8;
-	nums1[3] = 10;
-	nums1[4] = 12;
+	nums1[2] = 6;
+	// nums1[3] = 10;
+	// nums1[4] = 12;
 
+	// vector<int> nums2 = {2, 9, 11, 14};
+	vector<int> nums2 = {1, 2, 3};
 
+	merge(nums1, m, nums2, n);
 
-	vector<int> nums2 = {2, 9, 11, 14};
+	cout << "vals: " << endl;
 
-
-
-
+	for(int i = 0; i < nums1.size(); i++) {
+		cout << nums1[i] << ", ";
+	}
+	cout << endl;
 
 	return 0;
 }
 
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-	vector<int>::iterator it1 = nums1.end();
-	vector<int>::iterator it2 = nums2.end();
+    if(n == 0) return;
+    if(m == 0) nums1 = nums2;
 
-	cout << *it1 << endl;
+	vector<int>::iterator it1 = nums1.begin() + m - 1;
+	vector<int>::iterator it2 = nums2.end() - 1;
 
-	while(it1 != nums1.begin() && it2 != nums2.begin()) {
+	int size = nums1.size() - 1;
+	cout << "size: " << size << endl;
+	// bool is_inserted = false;
+
+	// for(; m >0, n > 0;) {
+	// 	if(nums1[m - 1] >= nums1[n - 1]) {
+	// 		nums1[size] = nums1[m - 1];
+	// 		nums1[m - 1] = 0;
+	// 		// is_inserted = true;
+	// 	}
+	// 	else {
+	// 		nums1[size] = nums2[n - 1];
+	// 		n--;
+
+			
+	// 	}
+	// 	size--;
+	// }
+	// if(n != 0) nums1[0] = nums2[0];
+
+
+
+	while(it1 >= nums1.begin() && it2 >= nums2.begin()) {
+		cout << "Here: " << *it1 << ", " << *it2 << endl;
+
+		if(*it1 >= *it2) {
+			nums1[size] = *it1;
+			*it1 = 0;
+			it1--;
+		}
+		else {
+			nums1[size] = *it2;
+			// *it2 = 0;
+			it2--;
+		}
+		size--;
+
 		
-
-
-		it1--;
-		it2--;
+	}
+	if(it2 >= nums2.begin()) {
+		while(it2 >= nums2.begin()) {
+			nums1[size] = *it2;
+			size--;
+			it2--;
+		}
 	}
 }
